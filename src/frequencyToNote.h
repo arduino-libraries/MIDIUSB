2bff2f4efc980eb2632430cd99e796f840a21416
agdl <a.guadalupi@arduino.cc>
2015-11-10 12:18:21 +0100

    Added two utility headers and fixed keyword highlight

---
+++ src/frequencyToNote.h
@@ -0,0 +1,108 @@
+#define NUMBER_OF_NOTES 88
+
+#define freqC8  4186.009
+
+#define freqB7  3951.066
+#define freqB7b 3729.310
+#define freqA7  3520.000
+#define freqA7b 3322.438
+#define freqG7  3135.963
+#define freqG7b 2959.955
+#define freqF7  2793.826
+#define freqE7  2637.020
+#define freqE7b 2489.016
+#define freqD7  2349.318
+#define freqD7b 2217.461
+#define freqC7  2093.005
+
+#define freqB6  1975.533
+#define freqB6b 1864.655
+#define freqA6  1760.000
+#define freqA6b 1661.219
+#define freqG6  1567.982
+#define freqG6b 1479.978
+#define freqF6  1396.913
+#define freqE6  1318.510
+#define freqE6b 1244.508
+#define freqD6  1174.659
+#define freqD6b 1108.731
+#define freqC6  1046.502
+
+#define freqB5  987.767
+#define freqB5b 932.328
+#define freqA5  880.000
+#define freqA5b 830.609
+#define freqG5  783.991
+#define freqG5b 739.989
+#define freqF5  698.456
+#define freqE5  659.255
+#define freqE5b 622.254
+#define freqD5  587.330
+#define freqD5b 554.365
+#define freqC5  523.251
+
+#define freqB4  493.883
+#define freqB4b 466.164
+#define freqA4  440.000
+#define freqA4b 415.305
+#define freqG4  391.995
+#define freqG4b 369.994
+#define freqF4  349.228
+#define freqE4  329.628
+#define freqE4b 311.127
+#define freqD4  293.665
+#define freqD4b 277.183
+#define freqC4  261.626
+
+#define freqB3  246.942
+#define freqB3b 233.082
+#define freqA3  220.000
+#define freqA3b 207.652
+#define freqG3  195.998
+#define freqG3b 184.997
+#define freqF3  174.614
+#define freqE3  164.814
+#define freqE3b 155.563
+#define freqD3  146.832
+#define freqD3b 138.591
+#define freqC3  130.813
+
+#define freqB2  123.471
+#define freqB2b 116.541
+#define freqA2  110.000
+#define freqA6b 103.826
+#define freqG2  97.999
+#define freqG2b 92.499
+#define freqF2  87.307
+#define freqE2  82.407
+#define freqE2b 77.782
+#define freqD2  73.416
+#define freqD2b 69.296
+#define freqC2  65.406
+
+#define freqB1  61.735
+#define freqB1b 58.270
+#define freqA1  55.000
+#define freqA1b 51.913
+#define freqG1  48.999
+#define freqG1b 46.249
+#define freqF1  43.654
+#define freqE1  41.203
+#define freqE1b 38.891
+#define freqD1  36.708
+#define freqD1b 34.648
+#define freqC1  32.703
+
+#define freqB0  30.868
+#define freqB0b 29.135
+#define freqA0  27.500
+
+const float noteFrequency[] = {freqC8,
+                               freqB7, freqB7b, freqA7, freqA7b, freqG7, freqG7b, freqF7, freqE7, freqE7b, freqD7, freqD7b, freqC7,
+                               freqB6, freqB6b, freqA6, freqA6b, freqG6, freqG6b, freqF6, freqE6, freqE6b, freqD6, freqD6b, freqC6,
+                               freqB5, freqB5b, freqA5, freqA5b, freqG5, freqG5b, freqF5, freqE5, freqE5b, freqD5, freqD5b, freqC5,
+                               freqB4, freqB4b, freqA4, freqA4b, freqG4, freqG4b, freqF4, freqE4, freqE4b, freqD4, freqD4b, freqC4,
+                               freqB3, freqB3b, freqA3, freqA3b, freqG3, freqG3b, freqF3, freqE3, freqE3b, freqD3, freqD3b, freqC3,
+                               freqB2, freqB2b, freqA2, freqA6b, freqG2, freqG2b, freqF2, freqE2, freqE2b, freqD2, freqD2b, freqC2,
+                               freqB1, freqB1b, freqA1, freqA1b, freqG1, freqG1b, freqF1, freqE1, freqE1b, freqD1, freqD1b, freqC1,
+                               freqB0, freqB0b, freqA0};

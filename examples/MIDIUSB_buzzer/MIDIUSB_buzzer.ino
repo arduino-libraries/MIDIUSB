311a3c978c99b4426c7143a580232b474f5269cb
Paulo Costa <me@paulo.costa.nom.br>
2017-02-09 22:19:16 -0200

    Added example to use buzzer (tone/noTone) for MIDI playback

---
+++ examples/MIDIUSB_buzzer/MIDIUSB_buzzer.ino
@@ -0,0 +1,99 @@
+/*
+ * MIDIUSB_buzzer.ino
+ *
+ * Author: Paulo Costa
+ */ 
+
+#include <MIDIUSB.h>
+#include "pitchToFrequency.h"
+
+#define BUZZ_PIN 9
+
+const char* pitch_name(byte pitch) {
+  static const char* names[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  return names[pitch % 12];
+}
+
+int pitch_octave(byte pitch) {
+  return (pitch / 12) - 1;
+}
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  tone(BUZZ_PIN, pitchFrequency[pitch]);
+
+  Serial.print("Note On: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  noTone(BUZZ_PIN);
+
+  Serial.print("Note Off: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  Serial.print("Control change: control=");
+  Serial.print(control);
+  Serial.print(", value=");
+  Serial.print(value);
+  Serial.print(", channel=");
+  Serial.println(channel);
+}
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  midiEventPacket_t rx = MidiUSB.read();
+  switch (rx.header) {
+    case 0:
+      break; //No pending events
+      
+    case 0x9:
+      noteOn(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+      break;
+      
+    case 0x8:
+      noteOff(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+      break;
+      
+    case 0xB:
+      controlChange(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //control
+        rx.byte3         //value
+      );
+      break;
+      
+    default:
+      Serial.print("Unhandled MIDI message: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+  }
+}
+

11c1eecba60218b42be3cc4b9ac6fd942595705a
jb60 <j.bark@web.de>
2022-03-25 19:56:54 +0100

    Update pitchToNote.h
    In the second to last line the entry "pitchA6b" should read "pitchA2b".

--- src/pitchToNote.h
+++ src/pitchToNote.h
@@ -101,6 +101,6 @@ const int notePitch[] = {pitchC8,
                                pitchB5, pitchB5b, pitchA5, pitchA5b, pitchG5, pitchG5b, pitchF5, pitchE5, pitchE5b, pitchD5, pitchD5b, pitchC5,
                                pitchB4, pitchB4b, pitchA4, pitchA4b, pitchG4, pitchG4b, pitchF4, pitchE4, pitchE4b, pitchD4, pitchD4b, pitchC4,
                                pitchB3, pitchB3b, pitchA3, pitchA3b, pitchG3, pitchG3b, pitchF3, pitchE3, pitchE3b, pitchD3, pitchD3b, pitchC3,
-                               pitchB2, pitchB2b, pitchA2, pitchA6b, pitchG2, pitchG2b, pitchF2, pitchE2, pitchE2b, pitchD2, pitchD2b, pitchC2,
+                               pitchB2, pitchB2b, pitchA2, pitchA2b, pitchG2, pitchG2b, pitchF2, pitchE2, pitchE2b, pitchD2, pitchD2b, pitchC2,
                                pitchB1, pitchB1b, pitchA1, pitchA1b, pitchG1, pitchG1b, pitchF1, pitchE1, pitchE1b, pitchD1, pitchD1b, pitchC1,
                                pitchB0, pitchB0b, pitchA0};
